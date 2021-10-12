a2493c21793d0265ff460f2a64fdbdb713cfbcc2
BYJRK <suomikoto@outlook.com>
2021-10-12 23:35:10 +0800

    json to sql

--- visualize.py
+++ visualize.py
@@ -1,18 +1,47 @@
 import json
+import sqlite3
 import matplotlib.pyplot as plt
+from rich.console import Console
+from rich.table import Table
 
 LABELS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 
-def load_json(filename='songs.json'):
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+def load_sql(filename='songs.json'):
+    con = sqlite3.connect('songs.db')
+    langs = ['国%', '外%', '纯%']
+    data = {
+        'total': {
+            'all': con.execute("SELECT COUNT(title) FROM songs").fetchone()[0],
+            'chn': con.execute(
+                f"SELECT COUNT(title) FROM songs WHERE lang LIKE '{langs[0]}'"
+            ).fetchone()[0],
+            'for': con.execute(
+                f"SELECT COUNT(title) FROM songs WHERE lang LIKE '{langs[1]}'"
+            ).fetchone()[0],
+            'ins': con.execute(
+                f"SELECT COUNT(title) FROM songs WHERE lang LIKE '{langs[2]}'"
+            ).fetchone()[0],
+        },
+        'count': {},
+    }
+
+    for key in LABELS:
+        data['count'][key] = [
+            con.execute(
+                "SELECT COUNT(title) FROM songs WHERE lang LIKE (?) AND key= (?)",
+                (lang, key),
+            ).fetchone()[0]
+            for lang in langs
+        ]
+
+    return data
 
 
 def pie(songs):
     fig, ax = plt.subplots()
     ax.pie(
-        [len([s for s in songs if s['key'] == l]) for l in LABELS],
+        [sum(songs['count'][key]) for key in LABELS],
         labels=LABELS,
         autopct='%1.1f%%',
         explode=[0.1, 0, 0.1, 0, 0.1, 0, 0, 0.1, 0, 0, 0, 0],
@@ -22,55 +51,42 @@ def pie(songs):
     fig.savefig('result.png')
 
 
-def table(songs):
-    """
-    Key   Total(percent)      Chinese        Foreign       Instrumental
-    -------------------------------------------------------------------
-    C      124 (11.69%)     57 ( 9.79%)     33 (10.15%)     34 (22.08%)
-    C#      49 ( 4.62%)     29 ( 4.98%)     10 ( 3.08%)     10 ( 6.49%)
-    D      126 (11.88%)     67 (11.51%)     47 (14.46%)     12 ( 7.79%)
-    D#      94 ( 8.86%)     58 ( 9.97%)     26 ( 8.00%)     10 ( 6.49%)
-    E      108 (10.18%)     57 ( 9.79%)     43 (13.23%)      8 ( 5.19%)
-    F       90 ( 8.48%)     53 ( 9.11%)     23 ( 7.08%)     14 ( 9.09%)
-    F#      61 ( 5.75%)     31 ( 5.33%)     24 ( 7.38%)      6 ( 3.90%)
-    G      135 (12.72%)     83 (14.26%)     26 ( 8.00%)     26 (16.88%)
-    G#      63 ( 5.94%)     32 ( 5.50%)     24 ( 7.38%)      7 ( 4.55%)
-    A       84 ( 7.92%)     52 ( 8.93%)     26 ( 8.00%)      6 ( 3.90%)
-    A#      70 ( 6.60%)     33 ( 5.67%)     22 ( 6.77%)     15 ( 9.74%)
-    B       57 ( 5.37%)     30 ( 5.15%)     21 ( 6.46%)      6 ( 3.90%)
-    -------------------------------------------------------------------
-    Total      1061              582           325             154
-    """
-    total = len(songs)
-    ctotal = len([s for s in songs if s['lang'] == '国语歌曲'])
-    ftotal = len([s for s in songs if s['lang'] == '外语歌曲'])
-    itotal = len([s for s in songs if s['lang'] == '纯音乐'])
-    res = "Key   Total(percent)      Chinese        Foreign       Instrumental\n"
-    res += '-' * 67 + '\n'
-    for key in LABELS:
-        ss = [s for s in songs if s['key'] == key]
-        sp = float(len(ss)) / total
-        chn = [s for s in ss if s['lang'] == "国语歌曲"]
-        cp = float(len(chn)) / ctotal
-        frn = [s for s in ss if s['lang'] == "外语歌曲"]
-        fp = float(len(frn)) / ctotal
-        ins = [s for s in ss if s['lang'] == "纯音乐"]
-        ip = float(len(ins)) / ctotal
-        res += f'{key:<4} '
-        res += f'{len(ss):>5} ({sp*100:>5.2f}%) '
-        res += f'{len(chn):>6} ({cp*100:>5.2f}%) '
-        res += f'{len(frn):>6} ({fp*100:>5.2f}%) '
-        res += f'{len(ins):>6} ({ip*100:>5.2f}%)\n'
-    res += '-' * 67 + '\n'
-    res += f'Total     {total:>5}            {ctotal:>5}         {ftotal:>5}           {itotal:>5}\n'
+def rich_table(songs):
+    table = Table(show_header=True)
+    table.add_column('Key', style='bold')
+    table.add_column('Total(percent)', justify='right')
+    table.add_column('Chinese', justify='right')
+    table.add_column('Foreign', justify='right')
+    table.add_column('Instrumental', justify='right')
 
-    print(res)
+    total = songs['total']['all']
+    ctotal = songs['total']['chn']
+    ftotal = songs['total']['for']
+    itotal = songs['total']['ins']
+    for key in LABELS:
+        ss = sum(songs['count'][key])
+        sp = float(ss) / total
+        chn, frn, ins = songs['count'][key]
+        cp, fp, ip = float(chn) / ctotal, float(frn) / ctotal, float(ins) / ctotal
+        table.add_row(
+            f'{key}',
+            f'{ss:>5} ({sp*100:>5.2f}%)',
+            f'{chn:>6} ({cp*100:>5.2f}%)',
+            f'{frn:>6} ({fp*100:>5.2f}%)',
+            f'{ins:>6} ({ip*100:>5.2f}%)',
+            end_section=key == 'B',
+        )
+    table.add_row(
+        'Total', str(total), str(ctotal), str(ftotal), str(itotal), style='bold'
+    )
+    console = Console()
+    console.print(table)
 
 
 def main():
-    songs = load_json()
-    pie(songs)
-    table(songs)
+    songs = load_sql()
+    # pie(songs)
+    rich_table(songs)
 
 
 if __name__ == '__main__':
